172644674cbdadf79255657add50bbb126b3d828
HDUysz <ysz@hdu.edu.cn>
2024-03-06 11:29:05 +0800

    update

--- code/56.cpp
+++ code/56.cpp
@@ -4,3 +4,45 @@
 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
 */
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+
+        sort(intervals.begin(), intervals.end(), compare);
+
+        vector<vector<int>> merged;
+        merged.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (merged.back()[1] >= intervals[i][0]) {
+                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+            } else {
+                merged.push_back(intervals[i]);
+            }
+        }
+
+        return merged;
+    }
+
+    static bool compare(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    }
+};
+
+int main() {
+    Solution solve;
+    vector<vector<int>> intervals = {{1,3},{2,6},{8,10},{15,18}};
+    vector<vector<int>> mergedIntervals = solve.merge(intervals);
+
+    for (auto v : mergedIntervals) {
+        for (auto i : v) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
--- update.bat
+++ update.bat
@@ -13,6 +13,6 @@ REM 拉取远程仓库的最新代码
 git pull
 
 REM 推送代码到远程的 master 分支
-git push origin master || git push origin main
+git push origin main
 
 echo 更新完成!
